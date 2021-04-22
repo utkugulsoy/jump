f33ac5b182f000620da31b38d26c29d0ee505f19
Utku Kaan Gülsoy <47040490+utkugulsoy@users.noreply.github.com>
2021-04-22 16:42:42 +0300

    Add files via upload

---
+++ MainCharacterController.cs
@@ -0,0 +1,123 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCharacterController : MonoBehaviour
+{
+    
+    Rigidbody rb;
+    public Vector3 jump;
+    public float jumpForce = 2.0f;
+    public bool isGrounded;
+    Vector3 v;
+    
+    
+    
+ 
+    float movementSpeed = 10;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        
+        
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isGrounded = true;
+        if (collision.gameObject.tag == "wall") {
+            isGrounded = false;
+        }
+        if (collision.gameObject.tag == "enemy") {
+            Destroy(gameObject);
+        }
+        
+
+
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if(collision.gameObject.tag=="wall")
+            isGrounded = true;
+    }
+
+
+    private void Update()
+    {
+
+       /* if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(jump * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }*/
+
+
+
+        if (Input.GetKey(KeyCode.D)) {
+
+            if (isGrounded)
+            {
+                v = rb.velocity;
+                if (rb.velocity.x > 5)
+                {
+                    v.x = 5f;
+                    rb.velocity = v;
+
+
+                }
+                rb.velocity = rb.velocity + transform.right * movementSpeed;
+            }
+
+
+            if (!isGrounded) {
+                v = rb.velocity;
+                v.x = -5f;
+                rb.velocity = v;
+                rb.velocity = rb.velocity + transform.right * movementSpeed;
+
+
+            }
+            Debug.Log(rb.velocity);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            if (isGrounded)
+            {
+                v = rb.velocity;
+                if (rb.velocity.x < -5)
+                {
+                    v.x = -5f;
+                    rb.velocity = v;
+                }
+
+                rb.velocity = rb.velocity + (-transform.right * movementSpeed);
+                Debug.Log(rb.velocity);
+            }
+
+            if (!isGrounded) {
+                v = rb.velocity;
+                v.x = 5f;
+                rb.velocity = v;
+                    rb.velocity= rb.velocity + (-transform.right * movementSpeed);
+                Debug.Log(rb.velocity);
+
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (isGrounded)
+            {
+               
+
+                rb.velocity = rb.velocity + transform.up * 8;
+                isGrounded = false;
+            }
+           
+            
+        }
+        
+        
+    }
+    
+}
